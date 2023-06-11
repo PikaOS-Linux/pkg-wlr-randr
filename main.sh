56078e09a6d6b260d29ff4d1f9a5457e0497611f
ferrreo <harderthanfire@gmail.com>
2023-06-11 18:33:32 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -9,9 +9,11 @@ add-apt-repository ppa:pikaos/pika
 add-apt-repository ppa:kubuntu-ppa/backports
 apt install pika-sources.deb --yes --option Acquire::Retries=5 --option Acquire::http::Timeout=100 --option Dpkg::Options::="--force-confnew"
 # Clone Upstream
-mkdir -p ./src-pkg-name
-cp -rvf ./debian ./src-pkg-name/
-cd ./src-pkg-name/
+
+git clone --recurse-submodules https://git.sr.ht/~emersion/wlr-randr
+cd wlr-randr
+cp -rvf ../debian ./
+
 
 # Get build deps
 ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
